4aa9179b6db70fd69bbb8a160fd770aa574f6440
SimonEiler <simon.eiler@gmail.com>
2024-01-03 16:07:16 +0100

    Add checkWinner function to assignments.js
    The functions calcAverage and checkWinner have been added to the assignments.js file. These functions calculate the average scores for 'Dolphins' and 'Koalas' and determine the winner based off these scores. Some unnecessary lines have been removed from the file.

--- 02-Fundamentals-Part-2/starter/assignments.js
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -19,19 +19,23 @@ console.log(appleJuice);
  */
 
 /////////////////////////////////////////////////////////////////////////
+/*
+const calcAverage = (score1, score2, score3) => (score1 + score2 + score3 / 3);
 
 const scoreDolphins = calcAverage(44, 23, 71);
 const scoreKoalas = calcAverage(65, 54, 49);
 
+const checkWinner = function (avDolphins, avgKoalas){
+    if (avDolphins >= 2 * avgKoalas) {
+        console.log(`Dolphin wins ${avDolphins} vs ${avgKoalas}`);
+    } else if (avgKoalas >= 2 * avDolphins) {
+        console.log(`Koalas wins ${avgKoalas} vs ${avDolphins}`);
+    } else {
+        console.log('No one wins!');
+    }
+}
 
-const malSchauen = 10;
-console.log(malSchauen);
+checkWinner(scoreDolphins, scoreKoalas);
+*/
 
-
-
-
-
-
-
-
-
+/////////////////////////////////////////////////////////////////////////
