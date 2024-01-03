0834c39012c543d76df2aa96373e41cb95908a2e
simonsway <simon.eiler@gmail.com>
2024-01-03 14:18:19 +0100

    Initial commit

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,149 @@
+//////////////////////////////////////////////////////////////////////////
+
+/*
+let country = 'Austria';
+let continent = 'Europe';
+let population = 8000000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+let isLand = false;
+let language;
+
+console.log(isLand);
+console.log(population);
+console.log(country);
+console.log(language);
+*/
+
+//////////////////////////////////////////////////////////////////////////
+
+/*
+const massMark = 78;
+const heightMark = 1.69;
+
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+
+console.log('BMI Mark - ' + BMIMark);
+console.log('BMI John - ' + BMIJohn);
+
+const markHigherBMI = BMIMark > BMIJohn;
+console.log(markHigherBMI);
+*/
+
+//////////////////////////////////////////////////////////////////////////
+
+/*
+const age = 15;
+
+if (age >= 18) {
+    console.log(`Sarah can start driving license `);
+} else {
+    const yearsLeft = 18 - age;
+    console.log(`Sarah is to young. Wait another ${yearsLeft} years:`)
+}
+ */
+
+//////////////////////////////////////////////////////////////////////////
+
+/*
+const birthYear = 1991;
+
+let century;
+if (birthYear <= 2000) {
+    century = 20;
+} else {
+    century = 21;
+}
+console.log(century);
+ */
+
+//////////////////////////////////////////////////////////////////////////
+
+/*
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+
+if (BMIMark > BMIJohn) {
+    console.log(`Marks BMI ${BMIMark} is higher then Johns ${BMIMark}`)
+} else {
+    console.log(`Johns BMI ${BMIJohn} ist higher then Marks ${BMIMark}`)
+}
+*/
+
+//////////////////////////////////////////////////////////////////////////
+
+/*
+// type conversion
+const inputYear = `1991`;
+console.log(Number(inputYear) + 18);
+console.log(String(23));
+
+// type coercion
+console.log('I am ' + 23 + ' years old');
+*/
+
+//////////////////////////////////////////////////////////////////////////
+
+// 5 falsy value: 0, '', undefined, null, NaN
+
+//////////////////////////////////////////////////////////////////////////
+
+/*
+const scoreDolphins = (88 + 91 + 400) / 3;
+const scoreKoalas = (88 + 91 + 400) / 3;
+
+if (scoreDolphins > scoreKoalas) {
+    console.log("Dolphins win the trophy");
+} else if (scoreKoalas > scoreDolphins) {
+    console.log("Koalas win the trophy");
+} else if (scoreKoalas === scoreDolphins) {
+    console.log("Both win the trophy")
+}
+ */
+
+//////////////////////////////////////////////////////////////////////////
+
+/*
+const bill = 275;
+const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+
+console.log(`Your bill was $ ${bill} thats why you have to tip ${tip} and the total value is ${bill + tip}`);
+ */
+
+//////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -0,0 +1,37 @@
+'use strict';
+//////////////////////////////////////////////////////////////////////////
+
+/*
+function logger() {
+    console.log(`My name is Simon`);
+}
+
+// calling / running / invoking function
+logger();
+
+function fruitProcessor(apples, oranges) {
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 2);
+console.log(appleJuice);
+ */
+
+/////////////////////////////////////////////////////////////////////////
+
+const scoreDolphins = calcAverage(44, 23, 71);
+const scoreKoalas = calcAverage(65, 54, 49);
+
+
+const malSchauen = 10;
+console.log(malSchauen);
+
+
+
+
+
+
+
+
+
